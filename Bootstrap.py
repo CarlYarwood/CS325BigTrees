5a0c6b78d3f08acc2dc05a1116161a15c1b281f3
Elijah Charleville <erc2181@truman.edu>
2018-11-12 23:00:12 -0600

    meh

---
+++ Bootstrap.py
@@ -0,0 +1,16 @@
+'''
+This file is responsible for running bootstrapping methods with our 
+UPGMA tree building algorithm
+
+Eli Charleville
+Nov 12 2018
+
+
+1. Align all sequences
+2. choose random columns, 
+    - create new set of sequences
+3. Build a new tree with these sequences 
+4. repeat until satisfied
+5. take statistics on clades to get confidence values 
+'''
+
