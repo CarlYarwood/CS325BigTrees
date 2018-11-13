69d71fa7166ee83689f58eeb06e750bf7461f9e9
CarlYarwood <cdy8858@truman.edu>
2018-11-13 17:03:11 -0600

    added string representations to Clade Class

--- Clade.py
+++ Clade.py
@@ -1,13 +1,15 @@
 class Clade:
-    def __init__(self, child1, child2, distance):
+    def __init__(self, child1, child2, distance, stringRep):
         self.child1 = child1
         self.child2 = child2
         self.distance = distance
+        self.stringRep = stringRep
     
     def getChild1Type(self):
         return type(self.child1)
     def getChild2Type(self):
         return type(self.child2)
+    
 
 class Leaf:
     def __init__(self, seqNum):
