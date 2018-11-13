6c4005f24b0e93b46a4ecf603993c8fcfba4ae55
elicharleville <erc2181@truman.edu>
2018-11-13 11:08:50 -0600

    clade got changed

--- Clade.py
+++ Clade.py
@@ -3,18 +3,16 @@ class Clade:
         self.child1 = child1
         self.child2 = child2
         self.distance = distance
-
     
-    def getChild1Type():
+    def getChild1Type(self):
         return type(self.child1)
-    def getChild2Type():
+    def getChild2Type(self):
         return type(self.child2)
 
 class Leaf:
     def __init__(self, seqNum):
         self.seqNum = seqNum
 
-    
-    def getSeqNum():
+    def getSeqNum(self):
         return self.seqNum
-    
+
