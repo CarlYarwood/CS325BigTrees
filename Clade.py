bcf77f9ad94b8988c1cbaaae560866580764bf87
elicharleville <erc2181@truman.edu>
2018-11-15 15:37:49 -0600

    fixed some syntax errors

--- Clade.py
+++ Clade.py
@@ -3,15 +3,15 @@ class Clade:
         self.child1 = child1
         self.child2 = child2
         self.distance = distance
-        self.stringRep = stringRep
         self.weight = child1.weight + child2.weight
     
     def getChild1Type(self):
         return type(self.child1)
     def getChild2Type(self):
         return type(self.child2)
-    def generateStringRep():
-        stringRep = "(" + child1.generateStringRep() + "," + child2.generateStringRep + ")"
+    def generateStringRep(self):
+        stringRep = "(" + self.child1.generateStringRep() + "," + \
+        self.child2.generateStringRep() + ")"
         return stringRep
     
 
@@ -23,7 +23,7 @@ class Leaf:
     def getSeqNum(self):
         return self.seqNum
 
-    def generateStringRep():
+    def generateStringRep(self):
         return str(self.seqNum)
     
 
